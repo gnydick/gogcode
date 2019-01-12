a53bd39c7becfdeefdd70e0be7ab187d8f3da575
Gabe E. Nydick <gnydick@nydick.net>
2019-01-12 14:18:14 -0800

    added a module to generate a lapping run.
    stick sandpaper to your bed and you're off to the races.

---
+++ lapping/lapping.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	. "github.com/gnydick/gogcode/gcode/utils"
+	"strings"
+)
+
+func main() {
+	speed := flag.Int("s", 200, "mm/sec desired (not actual)")
+	lines := flag.Int("n", 100, "number of randum strokes")
+	radius := flag.Float64("r", 1, "radius for movements")
+
+	jerk := flag.String("j", "750:2000", "jerk, printing:lapping")
+	flag.Parse()
+
+	fmt.Println("G90;\nG1 X155 Y200;")
+
+	jerkVals := strings.Split(*jerk, ":")
+
+	fmt.Println(fmt.Sprintf("M566 X%s Y%s;", jerkVals[1], jerkVals[1]))
+
+	for x := 1; x <= *lines; x++ {
+		GenRandMove(155, 200, radius, speed)
+	}
+
+	fmt.Println(fmt.Sprintf("M566 X%s Y%s;", jerkVals[0], jerkVals[0]))
+}
