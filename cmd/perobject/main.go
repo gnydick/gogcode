b9934ca42d4a2494c6857c2b0584ddfb66cb5abd
Gabe E. Nydick <gnydick@nydick.net>
2023-05-03 21:45:54 -0700

    stashing

--- cmd/perobject/main.go
+++ cmd/perobject/main.go
@@ -1,40 +1,47 @@
 package main
 
 import (
-	"bufio"
-	"os"
-
+	bufio "bufio"
+	flag "flag"
+	fmt "fmt"
 	. "github.com/gnydick/gogcode/pkg/structs"
 	. "github.com/gnydick/gogcode/pkg/utils"
+	os "os"
 )
 
 var tools = ToolSet{}
 
-func main() {
-	args := os.Args[1:]
-	input := args[0]
-	output := args[1]
-	//value_range := args[2]
+var state = State{}
+var cpuProfile = flag.String("cpuProfile", "", "write cpu profile to `file`")
+var memProfile = flag.String("memProfile", "", "write memory profile to `file`")
+var gcodeCommand = flag.String("gcodeCommand", "", "gcode command")
+var rangeStart = flag.Float64("rangeStart", 1.0, "value to start")
+var rangeEnd = flag.Float64("rangeEnd", 1.0, "value to end")
 
-	i, err := os.Open(input)
+var numObjects = flag.Int("numObjects", 1, "number of objects")
+var input = flag.String("input", "", "input file")
+var output = flag.String("output", "", "output file")
+
+func main() {
+	flag.Parse()
+
+	//var object_map map[string]float32
+
+	//var interval = (*rangeEnd - *rangeStart) / float64(*numObjects)
+
+	var value = *rangeStart
+	i, err := os.Open(*input)
 	Check(err)
 	scanner := bufio.NewScanner(i)
 
-	o, err := os.Create(output)
+	o, err := os.Create(*output)
 	Check(err)
 
 	bo := bufio.NewWriter(o)
 
-	//defer func() {
-	//	if err := o.Close(); err != nil {
-	//		panic(err)
-	//	}
-	//}()
-
 	defer func() {
 		i.Close()
-		bo.Flush()
-		o.Close()
+
 	}()
 
 	util := NewUtil()
@@ -44,10 +51,13 @@ func main() {
 		instructions := util.GenGcode(line)
 		for _, instruction := range instructions {
 			if instruction.Command == "M486" {
-				bo.WriteString((*instruction).Gcode() + "\n")
+				bo.WriteString(fmt.Sprintf("%s%f", value))
 			}
+			bo.WriteString((*instruction).Gcode() + "\n")
 
 		}
 	}
 	println("Hello!")
+	bo.Flush()
+	o.Close()
 }
